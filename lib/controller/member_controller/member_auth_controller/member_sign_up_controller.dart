6c2b14e12d31649b47fcf842ec6e9833f02cb01b
Ahad Khan <babaa336@gmail.com>
2023-01-10 08:08:14 +0500

    MEMBER MODULE COMPLETED.

--- lib/controller/member_controller/member_auth_controller/member_sign_up_controller.dart
+++ lib/controller/member_controller/member_auth_controller/member_sign_up_controller.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
-import 'package:the_powder_room_guys/view/screens/guest/guest_auth/guest_signup/guest_pass.dart';
 import 'package:the_powder_room_guys/view/screens/member/member_auth/member_signup/member_email.dart';
 import 'package:the_powder_room_guys/view/screens/member/member_auth/member_signup/member_name.dart';
+import 'package:the_powder_room_guys/view/screens/member/member_auth/member_signup/member_pass.dart';
 
 class MemberAuthController extends GetxController {
   static MemberAuthController instance = Get.find<MemberAuthController>();
@@ -10,7 +10,7 @@ class MemberAuthController extends GetxController {
   List<Widget> children = [
     MemberName(),
     MemberEmail(),
-    GuestPass(),
+    MemberPass(),
   ];
 
   void backButton() {
